f52b2ea08686108c168b7ae15fad14a4f3e4e41a
Ambika-Bhowmick <ambikab384@stemcollegiate.ca>
2025-04-08 16:55:29 -0600

    Update code.py

--- code.py
+++ code.py
@@ -1,40 +1,26 @@
 # This is the code for the obstacle course
 # Begin project code
 drivetrain.set_drive_velocity(75, PERCENT)
-drivetrain.drive_for(FORWARD, 600, MM)
-# This is the code for the cones
-drivetrain.drive_for(FORWARD, 180, MM)
-drivetrain.drive_for(LEFT, 47, DEGREES)
-drivetrain.drive_for(FORWARD, 25, MM)
-drivetrain.drive_for(RIGHT 94, DEGREES)
-drivetrain.drive_for(FORWARD, 25, MM)
-drivetrain.drive_for(LEFT, 47, DEGREES)
-drivetrain.drive_for(FORWARD, 25, MM)
-drivetrain.drive_for(RIGHT 47, DEGREES)
+drivetrain.drive_for(FORWARD, 440, MM)
+#These are the cones
+drivetrain.drive_for(FORWARD, 70, MM)
+drivetrain.drive_for(RIGHT,15, DEGREES)
+drivetrain.drive_for(FORWARD, 20, MM)
+drivetrain.drive_for(LEFT, 15, DEGREES)
+drivetrain.drive_for(FORWARD, 250, MM)
 # This is for going to the walls and turning for the walls
-drivetrain.drive_for(FORWARD, 250, MM)
+drivetrain.drive_for(FORWARD, 360, MM)
 drivetrain.drive_for(RIGHT 90, DEGREES)
-drivetrain.drive_for(FORWARD, 500, MM)
+drivetrain.drive_for(FORWARD, 210, MM)
 drivetrain.drive_for(RIGHT 90, DEGREES)
-#These are the second cones
-drivetrain.drive_for(FORWARD, 40, MM)
-drivetrain.drive_for(LEFT, 47, DEGREES)
-drivetrain.drive_for(FORWARD, 25 MM)
-drivetrain.drive_for(RIGHT 94, DEGREES)
-drivetrain.drive_for(FORWARD, 25 MM)
-drivetrain.drive_for(LEFT, 47, DEGREES)
 # This is the tunnel
-drivetrain.drive_for(FORWARD, 250, MM)
-drivetrain.drive_for(RIGHT 90, DEGREES)
+drivetrain.drive_for(FORWARD, 210, MM)
 # This is the seesaw
 drivetrain.set_drive_velocity(50, PERCENT)
-drivetrain.drive_for(FORWARD, 380, MM)
+drivetrain.drive_for(FORWARD, 440, MM)
+drivetrain.drive_for(RIGHT 90, DEGREES)
 # This is for the ending ramp
 drivetrain.set_drive_velocity(75, PERCENT)
-drivetrain.drive_for(FORWARD, 150, MM)
+drivetrain.drive_for(FORWARD, 440, MM)
 # This is the end
 drivetrain.set_stopping(BRAKE)
-
-
-
-
